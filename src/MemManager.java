134befcd432a06f0fa8e611b976fc467a9d58adc
Jae Trimboli <jaetrim@vt.edu>
2023-08-29 17:37:33 -0400

    mem manager

--- src/MemManager.java
+++ src/MemManager.java
@@ -1,22 +1,42 @@
 
-// Constructor. poolsize defines the size of the memory pool in bytes
-public class MemManager(int poolsize);
+public class MemManager {
 
-// Insert a record and return its position handle.
-// space contains the record to be inserted, of length size.
-Handle insert(byte[] space, int size);
+    // Constructor. poolsize defines the size of the memory pool in bytes
+    public MemManager(int poolsize) {
 
-// Return the length of the record associated with theHandle
-int length(Handle theHandle);
+    }
 
-// Free a block at the position specified by theHandle.
-// Merge adjacent free blocks.
-void remove(Handle theHandle);
 
-// Return the record with handle posHandle, up to size bytes, by
-// copying it into space.
-// Return the number of bytes actually copied into space.
-int get(byte[] space, Handle theHandle, int size);
+    // Insert a record and return its position handle.
+    // space contains the record to be inserted, of length size.
+    public Handle insert(byte[] space, int size) {
 
-// Dump a printout of the freeblock list
-void dump();
+    }
+
+
+    // Return the length of the record associated with theHandle
+    public int length(Handle theHandle) {
+
+    }
+
+
+    // Free a block at the position specified by theHandle.
+    // Merge adjacent free blocks.
+    public void remove(Handle theHandle) {
+
+    }
+
+
+    // Return the record with handle posHandle, up to size bytes, by
+    // copying it into space.
+    // Return the number of bytes actually copied into space.
+    public int get(byte[] space, Handle theHandle, int size) {
+
+    }
+
+
+    // Dump a printout of the freeblock list
+    public void dump() {
+
+    }
+}
