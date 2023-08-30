2b4f147d31216bafc31d08b2731d9253f91e7803
Jae Trimboli <jaetrim@vt.edu>
2023-08-30 19:05:49 -0400

    file reader

---
+++ src/FileReader.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FileReader {
+
+    public FileReader(String file) {
+        
+    }
+    
+    
+    private void readFile(String file) {
+        
+    }
+    
+}
