e8cee98f32861a6c0a4efc3da99408445d74bcf1
mohammadm147 <129554734+mohammadm147@users.noreply.github.com>
2023-08-31 18:56:24 -0400

    Added Pathing

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,17 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class FileReader {
 
-    public FileReader(String file) throws FileNotFoundException {
+    public FileReader(String file) throws FileNotFoundException, URISyntaxException {
         readFile(file);
     }
 
 
-    private void readFile(String fileName) throws FileNotFoundException {
-        File newFile = new File(fileName);
+    private void readFile(String fileName) throws FileNotFoundException, URISyntaxException {
+        URL path = ClassLoader.getSystemResource(fileName);
+        File newFile = new File(path.toURI());
         Scanner sc = new Scanner(newFile);
         while (sc.hasNextLine()) {
             System.out.println("asffFf");
