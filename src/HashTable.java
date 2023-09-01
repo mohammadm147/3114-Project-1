7a99a0227cc394cfd474e264bb443a87c7684b8d
mohammadm147 <129554734+mohammadm147@users.noreply.github.com>
2023-09-01 16:17:20 -0400

    update

--- src/HashTable.java
+++ src/HashTable.java
@@ -13,7 +13,10 @@ public class HashTable {
 
     private void insert(int id) {
         int key1 = (id % hashSize);
+        first key is location for insert
+        
         int key2 = ((id / hashSize) % (hashSize / 2) * 2) + 1;
+            add to the first key if the first position is filled
 
         seminar_count++;
     }
