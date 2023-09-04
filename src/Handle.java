c507cc7c8cf97b1880eb9adb3b14939de3716e2f
mohammadm147 <129554734+mohammadm147@users.noreply.github.com>
2023-09-04 15:24:15 -0400

    Update Handle and Hashtable

--- src/Handle.java
+++ src/Handle.java
@@ -4,10 +4,10 @@ public class Handle {
     private int key;
     private int id;
     
-    public Handle(int idInput, int keyInput)
+    public Handle()
     {
-        id = idInput;
-        key = keyInput;
+        key = 0;
+        id = 0;
     }
     
     public int getKey()
@@ -19,5 +19,14 @@ public class Handle {
     {
         return id;
     }
-
+    
+    public void setKey(int key1)
+    {
+        key = key1;
+    }
+    
+    public void setId(int id1)
+    {
+        id = id1;
+    }
 }
--- src/HashTable.java
+++ src/HashTable.java
@@ -3,32 +3,32 @@ public class HashTable {
 
     private int hashSize;
     private int seminar_count;
-    private Seminar[] hashTable;
+    private Handle[] hashTable;
 
     public HashTable(int hash_size) {
-        hashTable = new Seminar[hashSize];
+        hashTable = new Handle[hashSize];
         hashSize = hash_size;
         seminar_count = 0;
     }
 
 
-    private void insert(int id, Seminar sem) {
+    public void insert(int id, Handle handle) {
         if (seminar_count == (hashSize / 2)) {
             rehash();
         }
 
-        int key = keyFinder(id);
+        handle.setKey(keyFinder(id));
 
-        hashTable[key] = sem;
+        hashTable[handle.getKey()] = handle;
 
         seminar_count++;
     }
 
 
-    private void search(int id) {
+    public void search(int id) {
         // Prints the record with ID value if found
-        int key = keyFinder(id);
-        if (hashTable[key] != null) {
+        int key = (id % hashSize);
+        if (hashTable[key] == null) {
             System.out.print("Search FAILED -- There is no record with ID "
                 + id);
         }
@@ -36,8 +36,8 @@ public class HashTable {
     }
 
 
-    private void delete(int id) {
-        int key = keyFinder(id);
+    public void delete(int id) {
+        int key = (id % hashSize);
         if (hashTable[key] != null) {
             hashTable[key] = null;
         }
@@ -47,7 +47,7 @@ public class HashTable {
 
     private void rehash() {
         hashSize = hashSize * 2;
-        Seminar[] updated_hashtable = new Seminar[hashSize];
+        Handle[] updated_hashtable = new Handle[hashSize];
 
     }
 
