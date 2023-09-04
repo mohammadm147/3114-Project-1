9cf8107a67363f77bf4fc48c3164dbb24113cc67
mohammadm147 <129554734+mohammadm147@users.noreply.github.com>
2023-09-04 14:43:13 -0400

    Update Handle.java

--- src/Handle.java
+++ src/Handle.java
@@ -1,4 +1,15 @@
 
 public class Handle {
+    
+    private int key;
+    private int id;
+    
+    public Handle(int idInput, int keyInput)
+    {
+        id = idInput;
+        key = keyInput;
+    }
+    
+    
 
 }
