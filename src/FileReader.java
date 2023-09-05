790a0423eb5182e3fce631290bf39939d8736148
Jae Trimboli <jaetrim@vt.edu>
2023-09-05 13:31:19 -0400

    style

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,6 +1,4 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,20 +7,30 @@ public class FileReader {
 
     private ArrayList<Seminar> seminars;
 
+    /**
+     * General constructor for the FileReader class
+     * 
+     * @param file
+     *            takes in the name of the input file
+     * @throws Exception 
+     */
     public FileReader(String file)
-        throws FileNotFoundException,
-        URISyntaxException {
+        throws Exception {
         readFile(file);
     }
 
 
-    /*
+    /**
      * Parses through the input file and storing important information and using
      * the information to create Seminar objects
+     * 
+     * @param fileName
+     *            takes in the name of the input file
+     * @return a list of seminar objects after parsing the file
+     * @throws Exception 
      */
     public ArrayList<Seminar> readFile(String fileName)
-        throws FileNotFoundException,
-        URISyntaxException {
+        throws Exception {
         URL path = ClassLoader.getSystemResource(fileName);
         File newFile = new File(path.toURI());
         Scanner sc = new Scanner(newFile);
@@ -33,10 +41,10 @@ public class FileReader {
 
             // Deals with only insert functions in which the Seminar object is
             // created
-            String insert_line = sc.nextLine();
-            if (insert_line.contains("insert")) {
-                String[] insert = insert_line.split("\\s+");
-                int id = Integer.parseInt(insert[1]);
+            String currentLine = sc.nextLine();
+            if (currentLine.contains("insert")) {
+                String[] insertLine = currentLine.split("\\s+");
+                int id = Integer.parseInt(insertLine[1]);
                 String title = sc.nextLine();
                 String date = sc.next();
                 int length = Integer.parseInt(sc.next());
@@ -44,17 +52,29 @@ public class FileReader {
                 short y_coord = (short)Integer.parseInt(sc.next());
                 int cost = Integer.parseInt(sc.next());
                 sc.nextLine();
-                String keyword_line = sc.nextLine();
-                String[] keywords = keyword_line.split("\\s+");
+                String keywordLine = sc.nextLine();
+                String[] keywords = keywordLine.split("\\s+");
                 String description = sc.nextLine();
                 String format_des = description.replaceAll("\\s+", " ");
                 Seminar sem = new Seminar(id, title, date, length, x_coord,
                     y_coord, cost, keywords, format_des);
                 System.out.println(sem.toString());
+                int size = sem.serialize().length;
+                System.out.println("Size: " + size);
                 seminars.add(sem);
             }
-
-            // Search and Delete commands later
+            else if (currentLine.contains("search")) {
+                String[] search_line = currentLine.split("\\s+");
+                int id = Integer.parseInt(search_line[1]);
+            }
+            else if (currentLine.contains("print")) {
+                String[] print_line = currentLine.split("\\s+");
+                String command = print_line[1];
+            }
+            else if (currentLine.contains("delete")) {
+                String[] delete_line = currentLine.split("\\s+");
+                int id = Integer.parseInt(delete_line[1]);
+            }
         }
         sc.close();
         return seminars;
